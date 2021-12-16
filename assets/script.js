524e17cb92cc8b6cfdc08a27020fa8e3f6a46b1f
Giovanna Ruggio <giovannaruggio@gmail.com>
2021-12-16 11:03:38 -0500

    Utilize moment to sync time

---
+++ assets/script.js
@@ -0,0 +1,38 @@
+// GIVEN I am using a daily planner to create a schedule
+// WHEN I open the planner
+// THEN the current day is displayed at the top of the calendar
+// WHEN I scroll down
+// THEN I am presented with timeblocks for standard business hours
+// WHEN I view the timeblocks for that day
+// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+// WHEN I click into a timeblock
+// THEN I can enter an event
+// WHEN I click the save button for that timeblock
+// THEN the text for that event is saved in local storage
+// WHEN I refresh the page
+// THEN the saved events persist
+
+var today = moment().format("MMM Do, YYYY");
+$("#currentDay").text(today);
+
+
+var now = moment().hour();
+console.log(now);
+
+var textArea = $("textarea");
+console.log(textArea);
+
+for (var i=0; i < textArea.length; i++) {
+    console.log(textArea[i]);
+    console.log($(textArea[i]).attr("id"));
+    var timeBlockHour = $(textArea[i]).attr("id");
+    if (timeBlockHour === now) {
+        $(textArea[i]).addClass("present");
+    } else if (timeBlockHour < now) {
+        $(textArea[i]).addClass("past");
+    } else {
+        $(textArea[i]).addClass("future");
+    }
+}
+
+
